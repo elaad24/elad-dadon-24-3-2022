6ef35cda69ac868cf9d14e6f1ffee4229a9f7020
unknown <elaad2408@gmail.com>
2022-03-26 01:57:57 +0300

    added and implament  react router

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,31 @@ import "./css/App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import SpecificLocation from "./components/SpecificLocation";
 
-import fiveDaysweather from "./toDelete/mockData5days";
-import hoursweather from "./toDelete/mockData12hours";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+} from "react-router-dom";
+
+import Favourite from "./components/Favourite";
+
 function App() {
   return (
-    <div className="app">
-      <Home
-        fiveDaysData={fiveDaysweather.DailyForecasts}
-        hourWeatherData={hoursweather}
-        location={"Tel Aviv"}
-      />
+    <div className="app container d-flex">
+      <NavBar />
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favourite />} />
+
+          <Route path="/location/:id" element={<SpecificLocation />} />
+        </Routes>
+      </div>
     </div>
   );
 }
