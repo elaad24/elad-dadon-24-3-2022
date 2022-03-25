cca3c083ebfe119aad59535ea10d70a8ad359ac0
unknown <elaad2408@gmail.com>
2022-03-26 02:02:55 +0300

    add axios and add function to reqest data from the server

---
+++ src/services/appService.js
@@ -0,0 +1,20 @@
+import http from "./http";
+import { apiUrl } from "../config.json";
+const apikey = "UKpjmRddsUSGtOhUGlrZNaSNLe3mTiNe";
+
+export function oneDayForecast(locationID) {
+  return http.get(`${apiUrl}forecasts/v1/daily/1day/${locationID}?apikey=${apikey}&details=true&metric=true
+  `);
+}
+
+export function fiveDaysForecast(locationID) {
+  return http.get(
+    `${apiUrl}forecasts/v1/daily/5day/${locationID}?apikey=${apikey}&details=true&metric=true`
+  );
+}
+
+export function hourlyForecast(locationID) {
+  return http.get(
+    `${apiUrl}forecasts/v1/hourly/12hour/${locationID}?apikey=${apikey}&details=true&metric=true`
+  );
+}
