a6ed702e4ced858ae353a2a25dde0f821a3ec9bf
unknown <elaad2408@gmail.com>
2022-04-01 22:50:26 +0300

    Updates

--- src/components/common/NoFavourits.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function NoFavourits() {
-  return <h2>There isn't any saved locations</h2>;
-}
--- src/services/http.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const request = {
-  get: axios.get,
-};
-
-export default request;
