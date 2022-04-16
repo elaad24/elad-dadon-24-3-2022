ca15b4da3b226668ec59ce98225532741e6a2c50
unknown <elaad2408@gmail.com>
2022-04-17 01:49:38 +0300

    added if added location to favorits its save it to redux and will display it in spesific location page

--- src/components/SpecificLocation.jsx
+++ src/components/SpecificLocation.jsx
@@ -9,68 +9,96 @@ import { useSelector } from "react-redux";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-export default function Home() {
+export default function SpecificLocation() {
   const { id } = useParams();
   const isMetric = useSelector((state) => state.Settings.metricUnits);
 
   const [hourlyWeather, setHourlyWeather] = useState(null);
   const [fiveDayWeather, setFiveDayWeather] = useState(null);
+  const locationId = id;
+  const Favourites = useSelector((state) => state.Favourites);
+
+  let savedWeather;
+  let savedWeatherData;
 
   useEffect(async () => {
-    try {
-      const { data } = await hourlyForecast(id);
-      setHourlyWeather(data);
-    } catch (e) {
-      if (e.response.status === 400) {
-        toast.error("error - bad request ", {
-          position: "top-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-        });
-      } else if (e.response.status > 210) {
-        toast.warn("ererror - most probably run out of api credit ", {
-          position: "top-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-        });
+    //check if the weather is saved in favorits and if it does doesnt req from the server that data
+    savedWeather = Favourites.likedIds.includes(`${locationId}`);
+
+    //the data that saved for this sesific location from redux
+    savedWeatherData = Favourites.likedItems.filter(
+      (weatherItem) => weatherItem.id == locationId
+    );
+    console.log(savedWeatherData);
+  });
+
+  useEffect(async () => {
+    if (!savedWeather) {
+      try {
+        const { data } = await hourlyForecast(id);
+        setHourlyWeather(data);
+      } catch (e) {
+        if (e.response.status === 400) {
+          toast.error("error - bad request ", {
+            position: "top-right",
+            autoClose: 3500,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
+        } else if (e.response.status > 210) {
+          toast.warn("ererror - most probably run out of api credit ", {
+            position: "top-right",
+            autoClose: 3500,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
+        }
       }
+    } else {
+      console.log(savedWeatherData);
+      setHourlyWeather(savedWeatherData[0].hourWeatherData);
+      console.log("hourWeatherData added from redux ");
     }
   }, []);
 
   useEffect(async () => {
-    try {
-      const { data } = await fiveDaysForecast(id);
-      setFiveDayWeather(data);
-    } catch (e) {
-      if (e.response.status == 400) {
-        toast.error("error - bad request ", {
-          position: "top-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-        });
-      } else if (e.response.status > 210) {
-        toast.warn("ererror - most probably run out of api credit ", {
-          position: "top-right",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-        });
+    if (!savedWeather) {
+      try {
+        const { data } = await fiveDaysForecast(id);
+        setFiveDayWeather(data);
+      } catch (e) {
+        if (e.response.status == 400) {
+          toast.error("error - bad request ", {
+            position: "top-right",
+            autoClose: 3500,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
+        } else if (e.response.status > 210) {
+          toast.warn("ererror - most probably run out of api credit ", {
+            position: "top-right",
+            autoClose: 3500,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
+        }
       }
+    } else {
+      setFiveDayWeather(savedWeatherData[0].fiveDaysForcast);
+
+      console.log("five day weather added from redux ");
     }
   }, []);
 
@@ -78,7 +106,7 @@ export default function Home() {
     <>
       <ToastContainer
         position="bottom-left"
-        autoClose={5000}
+        autoClose={3500}
         hideProgressBar={false}
         newestOnTop={false}
         closeOnClick
@@ -89,7 +117,11 @@ export default function Home() {
       />
       {hourlyWeather && fiveDayWeather ? (
         <div className="column ">
-          <HomeInfo hourWeatherData={hourlyWeather} matricUnits={isMetric} />
+          <HomeInfo
+            hourWeatherData={hourlyWeather}
+            matricUnits={isMetric}
+            fiveDaysForcast={fiveDayWeather}
+          />
           <Chart hourWeatherData={hourlyWeather} matricUnits={isMetric} />
           <WeatherGroup fiveDaysForcast={fiveDayWeather?.DailyForecasts} />
         </div>
